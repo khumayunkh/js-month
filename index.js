3e5634327d7985d06426e27fd0173e9bacda5edd
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-22 17:39:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript supports several types of values
+
+x = 4              //number
+x = "hello world"  //string
+x = true           //boolen
+x = undefined      //"no value"
+x = null           //"no value" like undefined
