0782311aca68817980bd84100bf72a8477b0200e
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-22 17:51:06 +0500

    object and array

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ x = "hello world"  //string
 x = true           //boolen
 x = undefined      //"no value"
 x = null           //"no value" like undefined
+
+//Object
+
+let student =  {       
+    name : 'Humoyun', // key : name , value : Humoyun
+    age : 18          // key : age , value : 18
+}
+
+console.log(student.age)
+
+//Array 
+
+let array = [1, 2, 3, 5]
+
+console.log(array[0])  // 1: the first element of the array
+console.log(array.length)  // how many elements in the array
+array[4] = 6  // add a new element
+console.log(array[4])
