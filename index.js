b7fe74010cf1933019c7374f80a1b36ba3a1cba0
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-22 17:59:53 +0500

    object in array

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// JavaScript supports several types of values
+// JavaScript supports several types of values:
 
 x = 4              //number
 x = "hello world"  //string
@@ -6,20 +6,35 @@ x = true           //boolen
 x = undefined      //"no value"
 x = null           //"no value" like undefined
 
-//Object
+//Object:
 
 let student =  {       
     name : 'Humoyun', // key : name , value : Humoyun
     age : 18          // key : age , value : 18
 }
 
-console.log(student.age)
+//console.log(student.age)
 
-//Array 
+//Array: 
 
 let array = [1, 2, 3, 5]
 
-console.log(array[0])  // 1: the first element of the array
-console.log(array.length)  // how many elements in the array
+//console.log(array[0])  // 1: the first element of the array
+//console.log(array.length)  // how many elements in the array
 array[4] = 6  // add a new element
-console.log(array[4])
+//console.log(array[4])
+
+// Arrays and objects can hold other arrays and objects:
+
+let array = [
+    {name : 'Humoyun', age : 18}, // An array with 2 elements.
+    {name : 'Kamran', age : 23} // Each element is an object.
+]
+
+console.log(array[0].name) // => Humoyun
+
+let obg = {
+    numbers : [1, 2, 3, 4]
+}
+
+console.log(obj.numbers[2]) // => number 3
